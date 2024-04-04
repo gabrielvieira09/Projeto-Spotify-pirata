49e33793086db439b918fb478bfa176d41953d8f
Gabriel_Vieira <gabrielafonsovieira09@gmail.com>
2024-04-04 08:56:21 -0300

    Adicionando Rotas

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,6 +10,9 @@ export default function Home({navigation}) {
     title: "Higtway to hell",
     group: "AC/DC",
     album_image: "https://upload.wikimedia.org/wikipedia/pt/a/ac/Acdc_Highway_to_Hell.JPG",
+    album: "Hightway to Hell",
+    year: 1970,
+    genre: "Heavy Metal",
   }
   return (
     <View style={styles.constainer}>
---
+++ src/screens/MusicDetails.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function MusicDetails() {
+    return(
+        <View>
+            <Text>Detalhes da Música</Text>
+        </View>
+    );
+}
